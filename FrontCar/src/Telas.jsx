5c518201ec9b25d7e243d24c312461166c222e2f
Jau Italo <jau.batista@dcx.ufpb.br>
2023-09-15 16:27:12 -0300

    Dar um jeito no toggle

--- FrontCar/src/Telas.jsx
+++ FrontCar/src/Telas.jsx
@@ -1,61 +1,49 @@
 import React from 'react';
 import './LoginForm.css'; // Importe o arquivo CSS correspondente
 import './RegisterForm.css';
+import './overlay-container.css';
+import { FaFacebook, FaGoogle, FaLinkedin } from 'react-icons/fa'
 
 
 function LoginForm() {
   return (
+    /*========== Container ==========*/
+    <div className="login-container toggle" id="login-container">
 
-    <div className="login-container" id="login-container">
-    
+      {/*========== Login Container ==========*/}
       <div className="form-container">
         <form className="form form-login">
           <h2 className="form-title">Entrar com</h2>
-
-          <div className="form-social">
-            <a href="facebook" className="social-icon">
-              <i class="ri-facebook-fill"></i>
-            </a>
-
-            <a href="google" className="social-icon">
-              <i class="ri-google-fill"></i>
-            </a>
-
-            <a href="linkedin" className="social-icon">
-              <i class="ri-linkedin-fill"></i>
-            </a>
-          </div>
-
-          <p className="form-text">ou utilize sua conta</p>
+          <ul className='form-social'>
+            <li><FaFacebook /></li>
+            <li><FaGoogle /></li>
+            <li><FaLinkedin /></li>
+          </ul>
+          <p class="form-text">Ou utilize sua conta</p>
 
           <div className="form-input-container">
             <input type="email" className="form-input" placeholder="Email" />
             <input type="password" className="form-input" placeholder="Password" />
           </div>
-          
-          <a href="alguma-coisa" className="form-link">Esqueceu a senha?</a>
+
+          <a href=" " className="form-link">Esqueceu a senha?</a>
           <button type="button" className="form-button-logar">Logar</button>
           <p className="mobile-text">
             Não tem conta?
-            <a href="alguma-coisa" id="open-register-mobile">Registre-se</a>
+            <a href=" " id="open-register-mobile"> Registre-se</a>
           </p>
         </form>
+
+        {/*========== Register Container ==========*/}
         <form className="form form-register">
           <h2 className="form-title-register">Criar Conta</h2>
 
-          <div className="form-social-register">
-            <a href="alguma-coisa" className="social-icon">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="alguma-coisa" className="social-icon">
-              <i className="fab fa-google"></i>
-            </a>
-            <a href="alguma-coisa" className="social-icon">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-
-          <p className="form-text">ou cadastre seu email</p>
+          <ul className='form-social'>
+            <li><FaFacebook /></li>
+            <li><FaGoogle /></li>
+            <li><FaLinkedin /></li>
+          </ul>
+          <p class="form-text">ou cadastre seu email</p>
 
           <div className="form-input-container">
             <input type="text" className="form-input-register" placeholder="First Name" />
@@ -67,26 +55,30 @@ function LoginForm() {
 
           <button type="button" className="form-button-cadastro">Cadastrar</button>
           <p className="mobile-text">
-            Já tem conta?
-            <a href="alguma-coisa" id="open-login-mobile">Login</a>
+            Já tem conta? Faça
+            <a href=" " id="open-login-mobile"> Login</a>
           </p>
         </form>
       </div>
 
       <div className="overlay-container">
-        <div className="overlay">
-          <h2 className="form-title form-title-light">Já tem conta?</h2>
-          <p className="form-text">Para entrar na nossa plataforma faça login com suas informações</p>
-          <button className="form-button form-button-light" id="open-login">Entrar</button>
+        <div className="overlay-n2">
+        <h2 className="form-title">Já tem conta?</h2>
+          <p className="form-text">Se já é nosso cliente, clica abaixo e vem conferir nossos serviços</p>
+          <button className="form-button">Entrar</button>
+
         </div>
 
-        <div className="overlay">
-          <h2 className="form-title form-title-light">Olá Aluno!</h2>
-          <p className="form-text">Cadastre-se e comece a usar a nossa plataforma on-line</p>
-          <button className="form-button form-button-light" id="open-register">Cadastrar</button>
+        <div className="overlay-n1">
+          <h2 className="form-title">Olá Brother!</h2>
+          <p className="form-text">Cadastre-se e venha conhecer nossos serviços</p>
+          <button className="form-button">Cadastre-se</button>
         </div>
+
       </div>
-    </div>
+
+
+    </div >
   );
 }
 
