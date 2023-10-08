2bed04cf246a04603a5abeb4613ac624fd9c693e
Jau Italo <jau.batista@dcx.ufpb.br>
2023-10-08 15:40:13 -0300

    tentando implementar o axios

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home'
@@ -8,16 +9,17 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import NotFound from '../pages/NotFound'
 
+
 const Routers = () => {
     return <Routes>
-            <Route path="/" element={<Navigate to='/home' />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/cars" element={<CarListing />} />
-            <Route path="/cars/:slug" element={<CarDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Route path="/" element={<Navigate to='/home' />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cars" element={<CarListing />} />
+        <Route path="/cars/:slug" element={<CarDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
 }
 export default Routers
