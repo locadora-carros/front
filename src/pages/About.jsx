029bb6ffc64bf89f51a35088bf20b619cc1a3d4b
Jau Italo <jau.batista@dcx.ufpb.br>
2023-10-24 19:36:56 -0300

    dando continuidade

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from '../components/Helmet/Helmet'
+
+
+
 
 const About = () => {
-    return <div>About</div>;
+    return (
+        <Helmet title="About">
+            <CommonSection title="About" />
+        </Helmet>
+    )
 };
 export default About;
