fb93b7f5fffc74540848d85ad72f86e2b62cf715
Jau Italo <jau.batista@dcx.ufpb.br>
2023-10-26 22:38:43 -0300

    dando continuidade

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,28 +28,26 @@ const Header = () => {
     return <header className="header">
 
         {/* ========= Cabeça ========= */}
-        <div className="header-top">
+        <div className="header_top">
             <Container>
                 <Row>
                     <Col lg='6' md='6' sm='6'>
-                        <div className="logo">
-                            <h1>
-                                <Link to='/home' className="d-flex align-items-center gap-1">
-                                <i class="ri-roadster-line"></i>
-                                <span>Los Santos Company</span>
-                            </Link> 
-                            </h1>
+                        <div className="header_top_left">
+                            <span>Need Help?</span>
+                            <span className="header_top_help">
+                                <i class="ri-phone-fill"></i> +xx (xx)xxxxx-xxxx
+                            </span>
                         </div>
                     </Col>
-     
+
 
                     <Col lg='6' md='6' sm='6'>
-                        <div className="header-top-right d-flex align-items-center justify-content-end gap-3">
-                            <Link to="/login" className="d-flex align-items-center gap-1">
+                        <div className="header_top_right d-flex align-itens-center justify-content-end gap-3">
+                            <Link to="#" className="d-flex align-items-center gap-1">
                                 <i class="ri-login-circle-line"></i> Login
                             </Link>
 
-                            <Link to="/register" className="d-flex align-items-center gap-1">
+                            <Link to="#" className="d-flex align-items-center gap-1">
                                 <i class="ri-user-line"></i> Register
                             </Link>
                         </div>
@@ -57,6 +55,66 @@ const Header = () => {
                 </Row>
             </Container>
         </div>
+
+        {/*================== Meio =================*/}
+        <div className="header_middle">
+            <Container>
+                <Row>
+                    <Col lg="4" md="3" sm="4">
+                        <div className="logo">
+                            <h1>
+                                <Link to="/home" className=" d-flex align-items-center gap-1">
+                                    <i class="ri-car-line"></i>
+                                    <span>
+                                        Los Santos <br /> Service
+                                    </span>
+                                </Link>
+                            </h1>
+                        </div>
+                    </Col>
+
+                    <Col lg='3' md='3' sm='4'>
+                        <div className="header_location d-flex align-items-center gap-2">
+                            <span>
+                                <i class="ri-earth-line"></i>
+                            </span>
+                            <div className="header_location-content">
+                                <h4>Brasil</h4>
+                                <h6>João Pessoa, PB</h6>
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col lg='3' md='3' sm='4'>
+                        <div className="header_location d-flex align-items-center gap-2">
+                            <span>
+                                <i class="ri-time-line"></i>
+                            </span>
+                            <div className="header_location-content">
+                                <h4>Sunday to Sunday</h4>
+                                <h6>8am - 18pm</h6>
+                            </div>
+                        </div>
+                    </Col>
+
+
+                    <Col
+                        lg="2"
+                        md="3"
+                        sm="0"
+                        className=" d-flex align-items-center justify-content-end "
+                    >
+                        <button className="header_btn btn ">
+                            <Link to="/contact">
+                                <i class="ri-phone-line"></i> Request a call
+                            </Link>
+                        </button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+
+
         {/* ========= Navegação ========= */}
         <div className="main-navbar">
             <Container>
@@ -72,7 +130,7 @@ const Header = () => {
                                 <NavLink to={item.path} className={navClass => navClass.isActive ? 'nav-active  nav-item' : 'nav-item'}
                                     key={index}>
                                     {item.display}
-                                    </NavLink>
+                                </NavLink>
                             ))
                             }
                         </div>
